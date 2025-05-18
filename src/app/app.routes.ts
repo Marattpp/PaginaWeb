90a7d1be72701dd80fcb3286effcaabf24a1bc22
Marattpp <marat2.0v2@gmail.com>
2025-05-18 15:39:08 -0300

    Rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PedirComidaComponent } from './pages/pedir-comida/pedir-comida.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: "pedir-comida",
+        component: PedirComidaComponent
+    },
+    {
+        path: "perfil",
+        component: PerfilComponent
+    }
+];
