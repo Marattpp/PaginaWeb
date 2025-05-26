2f8bc7f447682514d427d7baaa58192c6ae4d88a
Marattpp <marat2.0v2@gmail.com>
2025-05-25 22:15:25 -0300

    ngIf e imagenes

--- src/app/core/components/tabs/tabs.component.ts
+++ src/app/core/components/tabs/tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -9,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class TabsComponent {
 
+  constructor (private router: Router){
+    this.router.events.subscribe(event =>{
+      if(event instanceof NavigationEnd){
+        switch (event.urlAfterRedirects){
+          case "":
+            this.seleccionado = [true,false,false];
+            break;
+          case "/pedir-comida":
+            this.seleccionado = [false,true,false];
+            break;
+          case "perfil":
+            this.seleccionado = [false,false,true];
+            break;
+          default:
+            this.seleccionado = [false,false,false];
+            break;
+        }
+      }
+    })
+  }
+
+  seleccionado = [false,false,false]
+
+  navegar(direccion:string) {
+    this.router.navigate([direccion])
+  }
 }
--- src/app/pages/pedir-comida/pedir-comida.component.ts
+++ src/app/pages/pedir-comida/pedir-comida.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pedir-comida',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pedir-comida.component.html',
   styleUrl: './pedir-comida.component.css'
 })
 export class PedirComidaComponent {
+  tipoOpcion: 'individual' | 'combo' | null = null;
 
+  seleccionarOpcion(opcion: 'individual' | 'combo'){
+    this.tipoOpcion = opcion;
+  }
 }
